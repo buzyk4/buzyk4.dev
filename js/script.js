7ef98d033af10a8c043211e20b22c531acc9dfd1
ArcadiusBuzyk <buzyka99@gmail.com>
2023-02-16 12:44:53 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 
+// Side menu buttons 
+
 document.querySelector('.burger-button').addEventListener('click', function () {
     document.querySelector('.animated-icon').classList.toggle('open');
     document.querySelector('.menu-container').classList.toggle('open');
