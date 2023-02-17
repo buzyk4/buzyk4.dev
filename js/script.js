2d81db1704056132e9c7e628304e826c1eeafd17
ArcadiusBuzyk <buzyka99@gmail.com>
2023-02-17 10:55:05 +0100

    dark and light mode test

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ document.querySelector('.burger-button').addEventListener('click', function () {
     document.querySelector('.animated-icon').classList.toggle('open');
     document.querySelector('.menu-container').classList.toggle('open');
 });
+
+//test
+
+function changeColor(color)
+{
+    var text = document.getElementById('test');
+    
+    text.style.color = color
+}
